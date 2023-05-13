5bff3463e5cb7d975024fe76e8006d19ea4e1719
andrew <cowslayer6789@gmail.com>
2023-05-12 21:56:24 -0400

    created entire mod

---
+++ src/main/java/com/slackow/joinautosprintmod/mixin/MinecraftClientMixin.java
@@ -0,0 +1,26 @@
+package com.slackow.joinautosprintmod.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.world.ClientWorld;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+    @Shadow @Final public GameOptions options;
+
+    @Inject(method = "joinWorld", at = @At("HEAD"))
+    public void onWorldJoin(ClientWorld world, CallbackInfo ci) {
+        KeyBinding sprintKey = options.sprintKey;
+        options.getSprintToggled().setValue(true);
+        if (!sprintKey.isPressed()) {
+            sprintKey.setPressed(true);
+        }
+    }
+}
